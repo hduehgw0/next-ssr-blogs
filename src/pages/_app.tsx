77031344919806e00764dac789dcfb0117c276a7
Kohei Nishiwaki <128705560+hduehgw0@users.noreply.github.com>
2026-04-27 02:15:57 +0900

    feat: 全ページ共通のレイアウト構造を実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Header } from "@/components/layouts/Header";
+import { Footer } from "@/components/layouts/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+
+      <main className="container mx-auto flex-1 px-4 py-8">
+        <Component {...pageProps} />
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
