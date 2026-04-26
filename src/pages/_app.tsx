6208610771e6f743ecbb5a77e81910c59b2d2bb4
Kohei Nishiwaki <128705560+hduehgw0@users.noreply.github.com>
2026-04-27 02:57:42 +0900

    feat: _appとindexにメタデータ初期設定を追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Header } from "@/components/layouts/Header";
 import { Footer } from "@/components/layouts/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex min-h-screen flex-col">
+      <Head>
+        <title>Algo Blog</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Qiitaライクなブログです。" />
+      </Head>
+
       <Header />
 
       <main className="container mx-auto flex-1 px-4 py-8">
